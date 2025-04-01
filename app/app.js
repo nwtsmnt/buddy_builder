b69a922218197dc6a73128cc62a67bd86e8aada1
maruftak <maruftak007@gmail.com>
2025-04-01 20:53:15 +0100

    updated

--- app/app.js
+++ app/app.js
@@ -2,17 +2,19 @@ const express = require("express");
 const app = express();
 const session = require("express-session");
 
+// Import all routes with consistent naming
 const authRoutes = require("./routes/auth_routes");
 const logoutRoutes = require("./routes/logout_routes");
 const postRoutes = require("./routes/post_routes");
 const aboutRoutes = require("./routes/about_routes");
 const introRoutes = require("./routes/intro_routes");
 const profileRoutes = require("./routes/profile_routes");
-const chatRoutes = require("./routes/chat_routes")
+const chatRoutes = require("./routes/chat_routes");
 const addPostRoutes = require("./routes/add_post_routes");
 const groupsRoutes = require("./routes/groups_routes");
 const editProfileRoutes = require('./routes/edit_profile_routes');
-const newsfeedRoutes = require('./routes/newsfeed_routes'); 
+const newsfeedRoutes = require('./routes/newsfeed_routes');
+const addPeopleRoutes = require('./routes/add_people_routes'); // Changed from add-people.js
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -20,6 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 // Serve static files like CSS
 app.use(express.static("./app/public"));
 
+// Set up session
 app.use(
     session({
         secret: "secret_key", // Replace with a strong secret key
@@ -33,7 +36,7 @@ app.use(
 app.set("view engine", "pug");
 app.set("views", "./app/views");
 
-// Use Routes
+// Register all routes (once each)
 app.use("/auth", authRoutes);
 app.use("/", logoutRoutes);
 app.use("/", postRoutes);
@@ -41,12 +44,11 @@ app.use("/", aboutRoutes);
 app.use("/", introRoutes);
 app.use("/profile", profileRoutes);
 app.use("/chat", chatRoutes);
-app.use("/profile", profileRoutes);
 app.use("/add_post", addPostRoutes);
-app.use("/", groupsRoutes);
+app.use("/groups", groupsRoutes);
 app.use("/", editProfileRoutes);
-app.use("/", newsfeedRoutes);
-
+app.use("/newsfeed", newsfeedRoutes);
+app.use("/add-people", addPeopleRoutes); // Keep URL path the same
 
 // Start server
 app.listen(3000, () => {
